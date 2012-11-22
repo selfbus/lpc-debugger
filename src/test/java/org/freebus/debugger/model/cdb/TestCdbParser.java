e291e8d2ad0fe321cd30fdbd75b17b9f6ac8c6c5
stefan <stefan.taferner@gmx.at>
2012-11-22 08:59:28 +0100

    Cleanup of the examples. Started implementing the .cdb parser.

---
+++ src/test/java/org/freebus/debugger/model/cdb/TestCdbParser.java
@@ -0,0 +1,31 @@
+package org.freebus.debugger.model.cdb;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class TestCdbParser
+{
+   @Test
+   public void testExample1() throws IOException, ParseException
+   {
+      File file = new File(getClass().getClassLoader().getResource("example1/out.cdb").getPath());
+      assertNotNull(file);
+      
+      CdbParser parser = new CdbParser(file);
+      parser.parse();
+
+      Map<String, Symbol> syms = parser.getSymbols();
+      assertNotNull(syms);
+      assertTrue(!syms.isEmpty());
+
+      Symbol sym = syms.get("portbuffer");
+      assertNotNull(sym);
+   }
+}
